9d35f136109bc42c87059b5a6d65050d0ef3b971
MiTnh26 <minhtnhe186338@fpt.edu.vn>
2026-03-02 14:36:45 +0700

    Revert "add useDebounce for reduce request for backend"
    This reverts commit 1f0875ef553402e461acc80950da82310531fc72.

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { useRef, useEffect, useState } from "react";
 
 const MOBILE_BREAKPOINT = 768
 
@@ -17,23 +16,4 @@ export function useIsMobile() {
   }, [])
 
   return !!isMobile
-};
-export default function useDebounce<T>(value: T, delay: number) {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const timeoutRef = useRef<any>(null);
-  const [debouncedValue, setDebouncedValue] = useState<T>(value);
-
-  useEffect(() => {
-    if (timeoutRef.current) clearTimeout(timeoutRef.current);
-
-    timeoutRef.current = setTimeout(() => {
-      setDebouncedValue(value);
-    }, delay);
-
-    return () => {
-      if (timeoutRef.current) clearTimeout(timeoutRef.current);
-    };
-  }, [delay, value]);
-
-  return debouncedValue;
 }
